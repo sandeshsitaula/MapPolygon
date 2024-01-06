8342775193ff3cd58f9d828bef5d28c278fc5a0d
sandeshsitaula <navaltajohan@gmail.com>
2024-01-06 14:21:37 +0545

    intial setup using docker-compose and dockerfile completed

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# # Check if the .env file exists, and create it if not
+# if [ ! -f .env ]; then
+#     echo "Creating .env file with default values..."
+#     echo "POSTGRES_DB=postgres" > .env
+#     echo "POSTGRES_USER=postgres" >> .env
+#     echo "POSTGRES_PASSWORD=postgres" >> .env
+# else
+#     echo ".env file found. Using existing environment variables."
+# fi
+
+# # Source the .env file to load environment variables
+# source .env
+sleep 1s
+# Check if migrations need to be applied
+python manage.py showmigrations | grep -q "0 "  # Checks if there are 0 unapplied migrations
+MIGRATION_STATUS=$?
+
+# Apply migrations if needed
+if [ $MIGRATION_STATUS -ne 0 ]; then
+    echo "Applying migrations..."
+    python manage.py migrate
+fi
+# Start the Django development server
+echo "Starting Django development server..."
+python manage.py runserver 0.0.0.0:8000
