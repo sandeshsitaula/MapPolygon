95bb617b10e06fb5c375ff0a44d06246b0869cc9
sandeshsitaula <navaltajohan@gmail.com>
2024-01-07 08:51:37 +0545

    used leaflet to draw map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState,useEffect } from 'react'
+import { useEffect } from 'react'
 import axios from 'axios'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//hmU6Dl3GcNZqWXt6Sy_Uz281sCHdfjGRxFO0bkJfuDU
+import {AddMap} from './pages/map'
+import {Home} from './pages/home';
 function App() {
-  const [count, setCount] = useState(0)
   useEffect(()=>{
     async function testing(){
 
@@ -21,26 +22,12 @@ testing()
   },[])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Router>
+    <Routes>
+       <Route path="/" element={<Home />} />
+      <Route path="/addMap" element={<AddMap />} />
+    </Routes>
+  </Router>
     </>
   )
 }
