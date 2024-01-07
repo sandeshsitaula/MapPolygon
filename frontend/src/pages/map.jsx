4c38d010929447e06016a415c782382ef3e7532f
sandeshsitaula <navaltajohan@gmail.com>
2024-01-07 10:00:48 +0545

    successfully got map latitudes and longitudes

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,10 +1,62 @@
-import React, {useState} from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from "leaflet";
+import React, {useState,useEffect} from "react";
+import L from "leaflet";
+import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";import { Icon } from "leaflet";
+import { useRef } from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+});
 
 export function AddMap() {
+   const [center, setCenter] = useState({ lat: 51.505, lng: -0.09 });
+   const [MapLayers,setMapLayers]=useState([])
+  const ZOOM_LEVEL = 12;
+  const mapRef = useRef();
+
+  const _created = (e) => {
+    console.log(e)
+     const { layerType, layer } = e;
+    if (layerType === "polygon") {
+      const { _leaflet_id } = layer;
+      setMapLayers((layers) => [
+        ...layers,
+
+        { id: _leaflet_id, latlngs: layer._latlngs[0] },
+      ]);
+    }
+
+  };
+useEffect(()=>{
+  console.log(MapLayers)
+})
+
   return (
-     <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }}>
+     <MapContainer center={center} zoom={ZOOM_LEVEL} style={{ height: '500px', width: '100%' }} ref={mapRef}>
+      <FeatureGroup>
+                <EditControl
+                  position="topright"
+                  onCreated={_created}
+                  draw={
+                    {
+                       rectangle: false,
+                    circle: false,
+                    circlemarker: false,
+                    marker: false,
+                    polyline: false,
+                    }
+                  }
+                />
+              </FeatureGroup>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'      />
