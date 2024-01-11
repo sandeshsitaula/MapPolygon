35d0e7dfcf07b61fb0dfd775af03801cb72ac5de
sandeshsitaula <navaltajohan@gmail.com>
2024-01-11 12:29:25 +0545

    updated urls and added customers.py also added axiosInstance

--- backend/djangoProject/settings.py
+++ backend/djangoProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'Mapapp',
+    'customerapp',
 ]
 
 MIDDLEWARE = [
--- backend/djangoProject/urls.py
+++ backend/djangoProject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('Mapapp.urls'))
+    path('api/map/',include('Mapapp.urls')),
+    path('api/customer/',include('customerapp.urls'))
 ]
