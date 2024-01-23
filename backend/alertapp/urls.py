9b13793e535ade0d71d5d3bf997d13b5a85530ab
sandeshsitaula <navaltajohan@gmail.com>
2024-01-23 20:03:24 +0545

    backend stub function and resolved function implemented

--- backend/alertapp/urls.py
+++ backend/alertapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('addAlert/',views.AddAlert),
     path('allAlert/',views.GetAllAlert),
+    path('resolveAlert/<alertId>/',views.ResolveAlert),
     ]
--- backend/alertapp/views.py
+++ backend/alertapp/views.py
@@ -3,11 +3,22 @@ from rest_framework.decorators import api_view
 import json
 from django.contrib.gis.geos import Point
 from .models import AlertModel,AlertServiceModel
+from customerapp.models import ServiceAddress
 from customerapp.serializers import CustomerSerializer,ServiceAddressSerializer
 from alertapp.serializers import AlertModelSerializer,AlertServiceModelSerializer
 from Mapapp.models import ServiceArea
 import requests
 import os
+
+
+
+def stubFunction(customerList,message):
+    try:
+        for customer in customerList:
+            print(customer.get('email')) # will later be used to send email
+    except Exception as e:
+        error=str(e)
+        print(error)
 
 # Create your views here.
 @api_view(['POST'])
@@ -24,7 +35,6 @@ def AddAlert(request):
             service_area=ServiceArea.objects.get(id=service_area_id)
             alert_service=AlertServiceModel(alert=alert,service_area=service_area)
             alert_service.save()
-
 
         return Response({'message':"Alert has been successfully saved"},status=201)
 
@@ -49,13 +59,48 @@ def GetAllAlert(request):
 
             allAlert.append(data)
 
-
         return Response({'data':allAlert},status=200)
 
 
     except Exception as e:
         error=str(e)
         return Response({'error':f"Unexpected Error occured {error}"},status=400)
+
+
+@api_view(['GET'])
+def ResolveAlert(request,alertId):
+    try:
+        alert=AlertModel.objects.get(id=alertId)
+        alert.status="RESOLVED"
+        alert.message="This alert has been resolved"
+        alert.save()
+
+        customerList=[]
+
+        #Get list of all alert services
+        alertServices=AlertServiceModel.objects.filter(alert=alert).order_by('-id')
+
+        #for the lsit of services get the corresponding service address to get customer list
+        for alertService in alertServices:
+            serviceAddress=ServiceAddress.objects.filter(service_area=alertService.service_area).order_by('-id')
+
+            #for getting individual service address to get customer data
+            for service in serviceAddress:
+                data=ServiceAddressSerializer(service).data['customer']
+                if data not in customerList:
+                    customerList.append(data)
+
+
+        #will run function for all the customers  (later can be made background task using celery)
+        stubFunction(customerList,alert.message)
+        print(customerList)
+        return Response({'message':"The Alert has been successfully resolved"},status=200)
+
+
+    except Exception as e:
+        error=str(e)
+        print(error)
+        return Response({'error':f"unexpected error occured"})
 
 
 
