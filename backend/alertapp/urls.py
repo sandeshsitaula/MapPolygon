20ffff49c44776a3a10de7975abf13dcfa24bba6
sandeshsitaula <navaltajohan@gmail.com>
2024-01-26 07:11:08 +0545

    removed test function

--- backend/alertapp/urls.py
+++ backend/alertapp/urls.py
@@ -3,7 +3,6 @@ from . import views
 urlpatterns=[
     path('addAlert/',views.AddAlert),
     path('allAlert/',views.GetAllAlert),
-    path('test/',views.test),
     path('resolveAlert/<alertId>/',views.ResolveAlert),
     path('sendMessage/<alertId>/',views.SendMessage),
     ]
