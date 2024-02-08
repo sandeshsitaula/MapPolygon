ac523b818c80fb1a8ad7f5008c1e393794a1756e
sandeshsitaula <navaltajohan@gmail.com>
2024-02-08 08:57:59 +0545

    tasks.py now doing adb check removed previously to run without checking

--- backend/alertapp/tasks.py
+++ backend/alertapp/tasks.py
@@ -2,18 +2,24 @@
 import os
 from taskiq_matrix.instance import broker
 from asgiref.sync import sync_to_async
-# from celery import shared_task
-# import os
-#
+import subprocess
+
+
 @broker.task()
 async def sendMail(number,message):
     #checks for adb and shellms
     await sync_to_async(os.system)('adb devices')
 
-    command=f"adb shell am startservice --user 0 -n com.android.shellms/.sendSMS -e contact {number} -e msg {message}"
-    await sync_to_async(os.system)(command)#runs the command
-    print("hello")
-    return "Successfully message sent"
+    checkIfAdbExists=await sync_to_async(subprocess.run)('adb shell service check isms',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+    if (checkIfAdbExists.stdout.find("Service isms: found")==-1):
+        print("ShellMs not found")
+        return "shellms not found"
+    else:
+        command=f"adb shell am startservice --user 0 -n com.android.shellms/.sendSMS -e contact {number} -e msg {message}"
+        await sync_to_async(os.system)(command)#runs the command
+        print("hello")
+        return "Successfully message sent"
 
 #
 #
